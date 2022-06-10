04300b7e7f76fcbcd047cb3d6b1bd943b41dfe81
takumi <hiatana72@gmail.com>
2022-06-10 21:08:48 +0900

    add path operation function

--- api/main.py
+++ api/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from api.routers import task, done
 
+app = FastAPI()
+app.include_router(task.router)
+app.include_router(done.router)
 
 @app.get("/hello")
 async def hello():
