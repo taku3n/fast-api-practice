ea3633e0d0cdc695071f0452969aaac09080c998
takumi <hiatana72@gmail.com>
2022-06-11 11:53:14 +0900

    Update put /tasks/{task_id} in CRUD

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,3 +1,4 @@
+import asyncio
 from tkinter.messagebox import NO
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -5,10 +6,20 @@ from sqlalchemy.ext.asyncio import AsyncSession
 import api.models.task as task_model
 import api.schemas.task as task_schema
 
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 from sqlalchemy import select
 from sqlalchemy.engine import Result
+
+
+async def create_task(
+    db: AsyncSession, task_create: task_schema.TaskCreate
+) -> task_model.Task:
+    task = task_model.Task(**task_create.dict())
+    db.add(task)
+    await db.commit()
+    await db.refresh(task)
+    return task
 
 
 async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
@@ -23,11 +34,20 @@ async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
     )
     return result.all()
 
-async def create_task(
-    db: AsyncSession, task_create: task_schema.TaskCreate
+
+async def get_task(db: AsyncSession, task_id: int) -> Optional[task_model.Task]:
+    result: Result = await db.execute(
+        select(task_model.Task).filter(task_model.Task.id == task_id)
+    )
+    task: Optional[Tuple[task_model.Task]] = result.first()
+    return task[0] if task is not None else None # 要素が一つあってもtupleで返却されるため、1つ目の要素を取り出す
+
+
+async def update_task(
+    db: AsyncSession, task_create: task_schema.TaskCreate, original: task_model.Task
 ) -> task_model.Task:
-    task = task_model.Task(**task_create.dict())
-    db.add(task)
+    original.title = task_create.title
+    db.add(original)
     await db.commit()
-    await db.refresh(task)
-    return task
+    await db.refresh(original)
+    return original
